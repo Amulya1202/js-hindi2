927e1c5dd26e993e638d0809883aec6879b0975c
Amulya1202 <amulyamishragola@gmail.com>
2025-11-04 08:18:15 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as newer version
+let name="hitesh"
+let age=18
+let isloggedIn=false
+// number =>power 53
+
+//string
+//big int
+//boolean
+//null=>standalone value
+//symbol=>unique find
+
+
+//object
+console.log(typeof null); //iska output object aata hai
+console.log(typeof undefined);//iska output undefined aata hai
+
