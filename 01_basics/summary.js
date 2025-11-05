222aa14e14d63135eb4c211e9fbc6cf8eb8325ea
Amulya1202 <amulyamishragola@gmail.com>
2025-11-05 05:40:48 +0000

    summary

---
+++ 01_basics/summary.js
@@ -0,0 +1,24 @@
+const score =100
+const scoreValue =100.3
+
+const loggedIn = false
+const outsidetemp=null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id);
+console.log(anotherid);
+console.log(id==anotherid);
+
+const heros=["ironman","america","Loki"];
+ let myobj={
+    name:"amulya",
+    age:22,
+}
+ const myfunction=function(){
+    console.log("hello");
+    
+ }
