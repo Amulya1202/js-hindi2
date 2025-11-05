4d9edf1c25a4a04073261d7ed7fbf06a9ba3b885
Amulya1202 <amulyamishragola@gmail.com>
2025-11-05 07:20:52 +0000

    memory allocation

--- 01_basics/summary.js
+++ 01_basics/summary.js
@@ -1,24 +1,49 @@
-const score =100
-const scoreValue =100.3
+// // const score =100
+// // const scoreValue =100.3
 
-const loggedIn = false
-const outsidetemp=null
+// // const loggedIn = false
+// // const outsidetemp=null
 
-let userEmail;
+// // let userEmail;
 
-const id=Symbol('123')
-const anotherid=Symbol('123')
+// // const id=Symbol('123')
+// // const anotherid=Symbol('123')
 
-console.log(id);
-console.log(anotherid);
-console.log(id==anotherid);
+// // console.log(id);
+// // console.log(anotherid);
+// // console.log(id==anotherid);
 
-const heros=["ironman","america","Loki"];
- let myobj={
-    name:"amulya",
-    age:22,
-}
- const myfunction=function(){
-    console.log("hello");
+// // const heros=["ironman","america","Loki"];
+// //  let myobj={
+// //     name:"amulya",
+// //     age:22,
+// // }
+// //  const myfunction=fuanction(){
+// //     console.log("hello");
     
- }
+// //  }
+
+// // //  ++++++++++++++++++++++++++++++++++++++++++++++++
+// // // +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// // // Stack(Primitive),Heap(Non-Primitive)
+
+// // let myyoutube="amulyamishra"
+
+// // let anothername=myyoutube
+// // anothername="ishika"
+
+// // console.log(myyoutube);
+// // console.log(anothername);
+
+
+// let userone={
+//    email:"user@gmail.com"
+//    upi:"user@ybl"
+// }
+// let usertwo=userone
+
+// usertwo.email="me@gmail.com"
+
+// console.log(userone.email);
+// console.log(usertwo.email);node 01_basics/summary.js
