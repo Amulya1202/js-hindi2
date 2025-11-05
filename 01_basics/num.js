a58055f123eb988692f5b2236bff6c45934b759c
Amulya1202 <amulyamishragola@gmail.com>
2025-11-05 09:13:06 +0000

    maths

---
+++ 01_basics/num.js
@@ -0,0 +1,40 @@
+// const score=400
+
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber=23.8966
+
+// console.log(othernumber.toPrecision(3));
+
+// const hundredfs=100000
+// console.log(hundredfs.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // MATHS
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.3));
+
+// console.log(Math.ceil(4.3));
+
+// console.log(Math.floor(4.9));
+
+console.log(Math.min(4,5,7,8));
+console.log((Math.random()*10)+1);
+
+const min=18
+const max=20
+console.log(Math.floor((Math.random() *(max-min+1))+min));
+
+
+
+
+
