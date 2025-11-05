4188904e8cfc357b993c84d433d9a5ceb6c31a13
Amulya1202 <amulyamishragola@gmail.com>
2025-11-05 09:42:50 +0000

    date

--- 01_basics/num.js
+++ 01_basics/num.js
@@ -27,12 +27,38 @@
 
 // console.log(Math.floor(4.9));
 
-console.log(Math.min(4,5,7,8));
-console.log((Math.random()*10)+1);
+// console.log(Math.min(4,5,7,8));
+// console.log((Math.random()*10)+1);
 
-const min=18
-const max=20
-console.log(Math.floor((Math.random() *(max-min+1))+min));
+// const min=18
+// const max=20
+// console.log(Math.floor((Math.random() *(max-min+1))+min));
+
+// let d=new Date()
+// console.log(d);
+// console.log(d.toString());
+// console.log(d.toDateString());
+// console.log(d.toISOString());
+// console.log(d.toJSON());
+// // date ek object hai
+
+// let mydate=new Date(2023,0,23)
+// console.log(mydate.toDateString());
+
+// let mytime=Date.now()
+// console.log(mytime);
+// console.log(Math.round(Date.now()/1000));
+
+
+let newd=new Date()
+console.log(newd);
+console.log(newd.getMonth());
+
+
+
+
+
+
 
 
 
