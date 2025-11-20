bffc753818c8e17274a3e0c1801ff646193544b1
Amulya1202 <amulyamishragola@gmail.com>
2025-11-20 07:17:42 +0000

    more on paste

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -1,36 +1,80 @@
-//singleton
+// // // //singleton
 
-//object literals
+// // // //object literals
 
-const mysym=Symbol("key1")
-
-
-const jsuser={
-    name:"hitesh",
-    "full name":"hitesh chaudhay",
-    [mysym]:"mykey1",
-    age:18,
-    email:"hitesh@gmail.com",
-    isloggedin:false,
-    lastlogin:["monday","saturday","sunday"]
-}
-// console.log(jsuser.email)
-// console.log(jsuser["email"]);
-// console.log(jsuser["full name"]);
-// jsuser.email="hietshchat@am"
-// Object.freeze(jsuser)
-// jsuser.email="hitesh@123"
-// console.log(jsuser);
+// // // const mysym=Symbol("key1")
 
 
-jsuser.greeting=function(){
-    console.log("hello user");
-}
-jsuser.greeting2=function(){
-    console.log(`hello js user,,${this.name}`);
+// const jsuser={
+//    name:"hitesh",
+//   "full name":"hitesh chaudhay",
+//    [mysym]:"mykey1",
+//         age:18,
+//   email:"hitesh@gmail.com",
+//    isloggedin:false,
+//    lastlogin:["monday","saturday","sunday"]
+//  }
+// // // // console.log(jsuser.email)
+// // // // console.log(jsuser["email"]);
+// // // // console.log(jsuser["full name"]);
+// // // // jsuser.email="hietshchat@am"
+// // // // Object.freeze(jsuser)
+// // // // jsuser.email="hitesh@123"
+// // // // console.log(jsuser);
+
+
+// // // jsuser.greeting=function(){
+// // //     console.log("hello user");
+// // // }
+// // // jsuser.greeting2=function(){
+// // //     console.log(`hello js user,,${this.name}`);
     
-}
-console.log(jsuser.greeting());
-console.log(jsuser.greeting2());
+// // // }
+// // // console.log(jsuser.greeting());
+// // // console.log(jsuser.greeting2());
 
+
+// // //*************** singelton
+
+// // // const tinderuser=new Object()
+
+// // //  tinderuser.id="1234abc"
+// // //  tinderuser.isloggedin=false
+// // //  tinderuser.name="sammy"
+
+// // //  console.log(tinderuser);
+// //  const regularuser={
+// //     email:"some&gmail.com",
+// //     fullname:{
+// //         userfullname:{
+// //             firstname:"amulya",
+// //             lastname:"mishra"
+// //         }
+// //     }
+// //  }
+
+// // console.log(regularuser.fullname.userfullname.firstname);
+
+//  const obj1={1:"a",2:"b"}
+//  const obj2={3:"c",4:"d"}
+
+//  const obj3=Object.assign({},obj1,obj2)
+
+// console.log(obj3);
+// const obj4={...obj1,...obj2}
+
+// console.log(obj3);
+
+// const users=[
+//      {
+//         id:1,
+//         email:"am@gmail.com"
+//     }
+// ]
+   
+
+// users[1].email
+// console.log(jsuseruser);
+
+// console.log(Object.keys(jsuser));
 
