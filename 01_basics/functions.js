a18f57eff9ac782abc14e03219de7017dcd59d73
Amulya1202 <amulyamishragola@gmail.com>
2025-11-21 07:17:48 +0000

    functions

---
+++ 01_basics/functions.js
@@ -0,0 +1,66 @@
+
+// function sayname(){
+//     console.log("h");
+//     console.log("i");   
+//     console.log("t");
+//     console.log("e");
+//     console.log("s");
+//     console.log("h");
+// }
+
+//sayname()
+
+// function addtwonumber(number1,number2){
+//     console.log(number1+number2);
+// }
+
+
+
+// function addtwonumber(number1,number2){
+//     let result=number1+number2;
+//     return result;
+// }
+
+// const result=addtwonumber(3,8)
+// console.log(result);
+
+
+// function loginusermessage(username="sam"){
+//     if(username === undefined){
+//         console.log("please enter a username");    
+//         return    
+//     }
+//     return `${username} just lgged in`
+// }
+// console.log(loginusermessage("hitesh"));
+// console.log(loginusermessage());
+
+
+// function calculateprice(...num1){
+//     return num1
+// }
+// // console.log(calculateprice(200,400,500));
+
+// const user={
+//     username:"hitesh",
+//     price:199
+// }
+
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);  
+// }
+
+// handleobject(user)
+
+// handleobject({
+//     username:"sam",
+//     price:499
+// })
+
+// const mynewarr=[200,400,500]
+
+// function returnsecondvalue(getarray){
+//     return getarray[2]
+// }
+// console.log(returnsecondvalue(mynewarr));
+
