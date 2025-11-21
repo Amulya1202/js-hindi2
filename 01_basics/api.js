793cf562afd54a3fd233d34886a437cd66c29453
Amulya1202 <amulyamishragola@gmail.com>
2025-11-21 06:39:18 +0000

    apis

---
+++ 01_basics/api.js
@@ -0,0 +1,16 @@
+const course={
+    coursename:"js hindi",
+    price:"999",
+    courseinstruc:"hitesh"
+}
+const {courseinstruc:ins}=course///Destructuring of objects
+
+console.log(ins);
+
+
+//format of json api
+{
+    "name":"hitesh",
+    "price":"free"
+
+}
